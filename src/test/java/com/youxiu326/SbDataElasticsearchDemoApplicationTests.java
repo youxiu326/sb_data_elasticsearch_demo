5de2aef6b68b79e462b1733fb4d5d965d776dc0f
youxiu326 <youxiu326@163.com>
2019-09-22 18:11:31 +0800

    使用原生查询

--- src/test/java/com/youxiu326/SbDataElasticsearchDemoApplicationTests.java
+++ src/test/java/com/youxiu326/SbDataElasticsearchDemoApplicationTests.java
@@ -2,17 +2,27 @@ package com.youxiu326;
 
 import com.youxiu326.entity.Article;
 import com.youxiu326.repositories.ArticleRepository;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.data.domain.Pageable;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -85,6 +95,66 @@ public class SbDataElasticsearchDemoApplicationTests {
         // 执行查询
         List<Article> articles = template.queryForList(query, Article.class);
         articles.forEach(it-> System.out.println(it));
+    }
+
+    /**
+     * 高亮查询 报错了
+     */
+    @Test
+    public void testNativeSearchQueryHighlight(){
+
+        String fild="content";
+        NativeSearchQuery nativeSearchQuery=new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.queryStringQuery("中国").defaultField(fild))
+                .withHighlightFields(new HighlightBuilder.Field(fild))
+                .build();
+        AggregatedPage<Article> list =  template.queryForPage(nativeSearchQuery, Article.class, new SearchResultMapper() {
+            @Override
+            public <T> AggregatedPage<T> mapResults(SearchResponse response, Class<T> clazz, Pageable pageable) {
+                List<Article> films = new ArrayList<>();
+                SearchHits hits = response.getHits();
+                for (SearchHit hit:hits){
+                    if(hits.getHits().length<=0){
+                        return null;
+                    }
+                    Article film=new Article();
+                    String hightLightMessage = hit.getHighlightFields().get(fild).fragments()[0].toString();
+                    film.setId(Long.parseLong(hit.getId()));
+                    film.setTitle(hit.getSource().get("title").toString());
+                    film.setContent(hit.getSource().get("content").toString());
+
+                    try {
+                        String setMethodName = parSetName(fild);
+                        Class<? extends Article> filmClass = film.getClass();
+                        Method setMethod = filmClass.getMethod(setMethodName,String.class);
+                        setMethod.invoke(film,hightLightMessage);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    films.add(film);
+
+                }
+                if (films.size()>0){
+                    return new AggregatedPageImpl<>((List<T>) films);
+                }
+                return null;
+            }
+        });
+
+        list.getContent().forEach(film -> System.out.println(film));
+    }
+
+
+
+    public String parSetName(String fieldName){
+        if (null == fieldName || "".equals(fieldName)) {
+            return null;
+        }
+        int startIndex = 0;
+        if (fieldName.charAt(0) == '_')
+            startIndex = 1;
+        return "set" + fieldName.substring(startIndex, startIndex + 1).toUpperCase()
+                + fieldName.substring(startIndex + 1);
     }
 
 }
